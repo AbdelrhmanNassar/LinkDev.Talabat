519d9e6f09ef70e04727b92bea8017ffd0dbc5a1
abdlrhman <abdoljkhj@gmail.com>
2024-10-05 22:52:53 +0300

    SomeChanges

--- LinkDev.Talabat.Api/Controllers/WeatherForecastController.cs
+++ LinkDev.Talabat.Api/Controllers/WeatherForecastController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace LinkDev.Talabat.Api.Controllers
 {
-    [ApiController]
+	[ApiController]
 	[Route("api/[controller]")]
 	public class WeatherForecastController : ControllerBase
 	{
@@ -29,6 +29,22 @@ namespace LinkDev.Talabat.Api.Controllers
 				Summary = Summaries[Random.Shared.Next(Summaries.Length)]
 			})
 			.ToArray();
+		}
+
+		[HttpPost(Name = "GetWeatherForecast")]
+		public IEnumerable<string> GetOther()
+		{
+			List<string> random = new List<string>() { "Abdo", "Mostafa", "Hazem" };
+			Random random1 = new Random();
+			return Enumerable.Range(1, 3).Select(num => random[random1.Next(random.Count)]);
+			//	return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+			//	{
+			//		Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+			//		TemperatureC = Random.Shared.Next(-20, 55),
+			//		Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+			//	})
+			//	.ToArray();
+			//}
 		}
 	}
 }
