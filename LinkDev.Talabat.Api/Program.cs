03e55753db9fedfe4fcfc13739f33fa2c9df5fab
abdlrhman <abdoljkhj@gmail.com>
2024-10-07 05:27:37 +0300

    Part 05 Apply Migrations (Update-Database)

--- LinkDev.Talabat.Api/Program.cs
+++ LinkDev.Talabat.Api/Program.cs
@@ -9,7 +9,9 @@ namespace LinkDev.Talabat.Api
 {
 	public class Program
 	{
-		public static void Main(string[] args)
+		//[FromServices]
+		//public static StoreContext StoreContext { get; set;}
+        public static   async Task Main(string[] args)
 		{
 			var webAppilcationBuilder = WebApplication.CreateBuilder(args);
 			
@@ -22,6 +24,8 @@ namespace LinkDev.Talabat.Api
 														// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 			webAppilcationBuilder.Services.AddEndpointsApiExplorer();
 			webAppilcationBuilder.Services.AddSwaggerGen();
+			
+		
 			//webAppilcationBuilder.Services.AddDbContext<StoreContext>(optionsBuilder =>
 			//{
 			//optionsBuilder.UseSqlServer(webAppilcationBuilder.Configuration.GetConnectionString("storeConnection"));
@@ -30,9 +34,40 @@ namespace LinkDev.Talabat.Api
 			//);
 			//	DependencyInjection.AddPersistanceServices(webAppilcationBuilder.Services,webAppilcationBuilder.Configuration);
 			webAppilcationBuilder.Services.AddPersistanceServices(webAppilcationBuilder.Configuration);
+	
+			#endregion
+		
+			var app = webAppilcationBuilder.Build();//build web application
+
+			#region update Database And ask for object form di container explicilitly
+			var scope = app.Services.CreateScope(); //you should dispose the scope after using it
+			var services = scope.ServiceProvider;
+			var storeContext = services.GetRequiredService<StoreContext>();
+
+			var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+			//var logger = services.GetRequiredService<ILogger<Program>>();
+
+			try
+			{
+				var pendingMigrations = storeContext.Database.GetPendingMigrations();
+				if (pendingMigrations.Any())
+					await storeContext.Database.MigrateAsync();
+
+			}catch (Exception ex)
+			{
+				var logger = loggerFactory.CreateLogger<Program>();
+				logger.LogError("An Error Happened during migration");
+
+			}
+			finally
+			{
+				storeContext.Dispose();
+			}
+
 			#endregion
 
-			var app = webAppilcationBuilder.Build();//build web application
+
+
 
 			// Configure the HTTP request pipeline.
 			#region determining MiddleWares before Running = Configure Kestral Middleware 
