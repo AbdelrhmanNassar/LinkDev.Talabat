15bb2ccf660ae3f3610d2fa57effd4eb0f8c18f7
abdlrhman <abdoljkhj@gmail.com>
2024-10-07 21:10:42 +0300

    Part 09 Unit Of Work

---
+++ LinkDev.Talabat.Core.Domain/Contracts/IUnitOfWork.cs
@@ -0,0 +1,19 @@
+﻿using LinkDev.Talabat.Core.Domain.Enities.Product;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkDev.Talabat.Core.Domain.Contracts
+{
+    public interface IUnitOfWork : IAsyncDisposable
+    {
+        public IGenericRepository<Product, int> ProductRepo { get;}
+        public IGenericRepository<ProductBrand, int> productBrand { get;}
+        public IGenericRepository<ProductCategory, int> ProductCategoery { get; }
+
+        Task<int> CompeletAsnc();
+             
+    }
+}
---
+++ LinkDev.Talabat.Infrastructure.Peresistance/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,40 @@
+﻿
+using LinkDev.Talabat.Core.Domain.Contracts;
+using LinkDev.Talabat.Core.Domain.Enities.Product;
+using LinkDev.Talabat.Infrastructure.Peresistance.Repositries;
+using LinkDev.Talabat.Infrastrucutre.Infrastructure.Date;
+
+namespace LinkDev.Talabat.Infrastructure.Peresistance.UnitOfWork
+{
+	internal class UnitOfWork : IUnitOfWork
+
+	{
+		private readonly StoreContext _storeContext;
+		private readonly Lazy<IGenericRepository<Product, int>> _productRepo;
+		private readonly Lazy<IGenericRepository<ProductBrand, int>> _brandtRepo;
+		private readonly Lazy<IGenericRepository<ProductCategory, int>> _categoryRepo;
+		public UnitOfWork(StoreContext storeContext)
+        {
+            _storeContext = storeContext;
+			_productRepo = new Lazy<IGenericRepository<Product, int>>( ()=> new GenericRepo<Product,int>(_storeContext) );
+			_brandtRepo = new Lazy<IGenericRepository<ProductBrand, int>>( ()=> new GenericRepo<ProductBrand, int>(_storeContext) );
+			_categoryRepo = new Lazy<IGenericRepository<ProductCategory, int>>( ()=> new GenericRepo<ProductCategory, int>(_storeContext) );
+
+        }
+        public IGenericRepository<Product, int> ProductRepo { get => _productRepo.Value; }
+		public IGenericRepository<ProductBrand, int> productBrand { get => _brandtRepo.Value;  }
+		public IGenericRepository<ProductCategory, int> ProductCategoery { get => _categoryRepo.Value; }
+
+
+
+		public Task<int> CompeletAsnc()
+		{
+			throw new NotImplementedException();
+		}
+
+		public ValueTask DisposeAsync()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
