6bd51addad747c1909c766a304c3521836db826d
abdlrhman <abdoljkhj@gmail.com>
2024-10-17 21:08:48 +0300

    Part 01 Identity Module - Entities

---
+++ LinkDev.Talabat.Core.Domain/Enities/Identity/Address.cs
@@ -0,0 +1,22 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkDev.Talabat.Core.Domain.Enities.Identity
+{
+	internal class Address
+	{
+        public int Id { get; set; }
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
+        public required string City { get; set; }
+        public required string Country { get; set; }
+        public required string Street{ get; set; }
+
+        public  int UserId { get; set; }
+        public required ApplicationUser User { get; set; } //Actualy i don't need this but because i want to raise an error
+    }
+}
---
+++ LinkDev.Talabat.Core.Domain/Enities/Identity/ApplicationUser.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkDev.Talabat.Core.Domain.Enities.Identity
+{
+	internal class ApplicationUser : IdentityUser<int>  //key will be string
+	{
+        public required string DisplayName { get; set; }
+        public Address? Address { get; set; }
+
+    }
+}
